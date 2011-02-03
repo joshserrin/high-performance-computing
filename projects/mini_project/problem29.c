3d734adab3d7bb0688ecd1ab18bdc0009dd66483
Josh Serrin <josh.serrin@gmail.com>
2011-02-02 21:18:24 -0500

    finished problem 29.  Possible roomy bug with size

--- projects/mini_project/problem29.c
+++ projects/mini_project/problem29.c
@@ -1,63 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include <roomy.h>
+#include <RoomyArray.h>
+#include <RoomyList.h>
 
-#define COUNT 10000
+RoomyArray *x;
+RoomyList *y;
 
-int min(int *arr);
-int max(int *arr);
-int greater(const int a, const int b);
-int lesser(const int a, const int b);
-int fold(int *arr, int initial,	int (*compare)(const int, const in));
-
-int main(void) {
-	printf("\n\n");
-	srand( time(NULL) );
-
-	int x[COUNT];
-	int y[COUNT];
-	int i;
-	for(i = 0; i < COUNT; i++)
-	{
-		x[i] = nextInt();
-	}
-
-	printf("min=%d\n", min(x));
-	printf("max=%d\n", max(x));
-
-	printf("\n\n");
+void addElement(uint64 i, void* oldVal, void* newValOut) {
+	*(uint64*)newValOut = 9999-i;
 }
+void addToY(uint64 i, void *arrVal, void *passedVal) {
+	// arrVal needs to be added to y
+	RoomyList_add(y, arrVal);
+}
+void getXsubXi(uint64 i, void *val) {
+	// Add x[val] to y
+	// val is already x[i]
+	RoomyArray_access(x, *(uint64 *)val, NULL, addToY);
+	
+}
+void main(int argc, char **argv) {
+	Roomy_init(&argc, &argv);
 
-int nextInt() {
-	return rand() % COUNT + 1;
-}
-int min(int *arr) {
-	int min = fold(arr, INFINITY, lesser);
-	return min;
-}
-int greater(const int a, const int b) {
-	if(a > b) { return a; }
-	else return b;
-}
-int max(int *arr) {
-	int max = fold(arr, -INFINITY, greater);
-	return max;
-}
-int lesser(const int a, const int b) {
-	if(a < b) { return a; }
-	else return b;
-}
-// arr = the array
-// initial = the initial value to compare to
-// compare = Take two arguments, the first is the current answer (i.e. total)
-// 		and the second is the current value of the array
-// return the integer value that satisfies the function compared to all
-// 	other elements in the array
-int fold(int *arr, int initial,	int (*compare)(const int, const in)) {
-	int current = initial;
-	int i;
-	for(i=0; i<COUNT; i++) {
-		current = compare(current, arr[i]);
-	}
-	return current;
+	uint64 numElts = 10000;
+	x =  RoomyArray_makeBytes("x", sizeof(uint64), numElts);
+
+	// Populate the RA with values from 9999 to 0	
+	RoomyArray_mapAndModify(x, addElement);
+
+	y = RoomyList_make("y", sizeof(uint64));
+
+	// Populate the RL with x[x[i]]
+	// 0 indicated NULL is passed
+	RoomyArray_registerAccessFunc(x, addToY, 0);
+	RoomyArray_map(x, getXsubXi);
+
+	RoomyArray_sync(x);
+	RoomyList_sync(y);
+
+	printf("Num elts in x: %llo\n", RoomyArray_size(x));
+	printf("Num elts in y: %llo\n", RoomyList_size(y));
+	printf("\n");
 }
--- projects/mini_project/run.sh
+++ projects/mini_project/run.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+ROOMY_INSTALL=/home/jserrin/roomy-install
+
 rm a.out
-gcc $1
+#gcc $1 -I/home/jserrin/roomy-install/include 
+mpicc $1 -o a.out -I$ROOMY_INSTALL/include -L$ROOMY_INSTALL/lib $ROOMY_INSTALL/lib/libroomy.a -lmpich -lpthread -lm
 ./a.out
 
 exit 0;
